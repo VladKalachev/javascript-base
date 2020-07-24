6f0a6ca4b4c0aec15260e3ccdadd1e3ebcbcd9c1
Corvax <vlvete1990@yandex.ru>
2020-07-24 19:08:05 +0300

    init

---
+++ types.js
@@ -0,0 +1,20 @@
+// Namber
+// let a = 10
+// let b = 20.10
+// let c = a ** b
+// // console.log(c)
+// // Math
+// let d = Math.floor(2.5)
+// // console.log(d)
+// let f = 12%5
+// // console.log(f)
+// let max = Number.MAX_VALUE
+// let min = Number.MIN_VALUE
+// // console.log(max)
+// // console.log(min)
+// console.log(0.1 + 0.2)
+
+//BigInt
+let a = 10n
+let b = BigInt(200)
+console.log(typeof b)
