e4131ab689f626329c5083a2aee04b02d02cea35
Corvax <vlvete1990@yandex.ru>
2020-07-26 16:42:40 +0300

    chore: add typeof

--- types.js
+++ types.js
@@ -14,7 +14,60 @@
 // // console.log(min)
 // console.log(0.1 + 0.2)
 
-//BigInt
-let a = 10n
-let b = BigInt(200)
-console.log(typeof b)
+// BigInt
+// let a = 10n
+// let b = BigInt(200)
+// console.log(typeof b)
+
+// String
+// let a = "Name"
+// let b = 'Name'
+// let c = `Name ${b}`
+// // console.log(c)
+// console.log(c.toUpperCase())
+
+// Boolean
+// let a = false
+// let b = true
+// let c = 10 > 2
+// console.log(c)
+
+// Undefined
+// let a = undefined
+// console.log(a)
+
+// Null
+// let a = null 
+
+// Symbol
+// let uid = Symbol("uid")
+// let uid2 = Symbol("uid")
+// console.log(uid === uid2)
+
+// Object
+let object = new Object({name: "Test"})
+let obj2 = {
+    name: "Test"
+}
+// console.log(object.name)
+// console.log(obj2.name)
+
+// object.name = "Test2"
+// obj2.age = 20
+// console.log(object)
+// console.log(obj2)
+// delete obj2.age
+// console.log(obj2)
+
+// typeof
+console.log(typeof 10)
+console.log(typeof 10n)
+console.log(typeof 10.20)
+console.log(typeof "Text")
+console.log(typeof Symbol("uid"))
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof{})
+console.log(typeof [])
+console.log(typeof function(){})
